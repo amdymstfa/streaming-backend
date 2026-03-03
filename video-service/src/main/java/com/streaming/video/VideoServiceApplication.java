14c8503c762e611730d751e67dc08e0fa50ddb13
amdymstfa <amdymoustapha011@gmail.com>
2026-03-03 14:46:33 +0000

    feat(video): add @EnableDiscoveryClient to VideoServiceApplication

--- video-service/src/main/java/com/streaming/video/VideoServiceApplication.java
+++ video-service/src/main/java/com/streaming/video/VideoServiceApplication.java
@@ -2,12 +2,12 @@ package com.streaming.video;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class VideoServiceApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(VideoServiceApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(VideoServiceApplication.class, args);
+  }
 }
